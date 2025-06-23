b1afbc421d1504080a52845a1979a33ee4c81569
Amir-Shaban32 <amirshaban973@gmail.com>
2025-06-23 16:14:47 +0300

    add text file

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
+
 const readline = require('readline');
+const fs = require('fs');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-
-rl.question('User name: ', async (USERNAME) => {
+rl.question('GitHub username: ', async (USERNAME) => {
   try {
     const response = await fetch(`https://api.github.com/users/${USERNAME}/repos`);
     const data = await response.json();
 
     if (!Array.isArray(data)) {
       console.error('Error:', data.message || 'Unexpected response');
-    }
-    else {
+    } else {
       const repoNames = data.map(repo => repo.name);
-      console.log('repos: ', repoNames);
+      console.log('Repositories:', repoNames);
+
+      fs.writeFile('repoNames.txt', repoNames.join('\n'), (err) => {
+        if (err) {
+          console.error('File write error:', err.message);
+        } else {
+          console.log('Repo names saved to repoNames.txt');
+        }
+      });
     }
-  }
-  catch (err) {
-    console.error('Fetch error', err.message);
-  }
-  finally {
+  } catch (err) {
+    console.error('Fetch error:', err.message);
+  } finally {
     rl.close();
   }
 });
+
